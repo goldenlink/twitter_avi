ad1e319852d66e11a1f11ec0cc26e874c1733377
Aurélien VIOLETTE <aurelien.violette@free.fr>
2011-07-25 18:40:44 +0300

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
+require 'digest'
 
 class User < ActiveRecord::Base
   attr_accessor :password
@@ -23,4 +14,52 @@ class User < ActiveRecord::Base
   validates :password,  :presence     => true,
                         :confirmation => true,
                         :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches witht he submitted password
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email,submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+
 end
+
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
+
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name "Daria VIOLETTE"
+  user.email "d.violette@gmail.com"
+  user.password "foobar"
+  user.password_confirmation "foobar"
+end
