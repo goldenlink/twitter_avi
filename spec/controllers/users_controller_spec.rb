43d014c3e0f555337d5288ad0086d7d576233df0
Aurélien VIOLETTE <aurelien.violette@free.fr>
2011-07-26 12:22:54 +0300

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -49,4 +49,55 @@ describe UsersController do
 
   end
 
+  describe "POST 'create'" do
+
+    describe "success" do
+      before(:each) do
+        @attr = { :name =>  "New User", :email => "user1@example.com", 
+          :password => "foobar", :password_confirmation => "foobar" }
+      end
+
+      it "should create a new user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+
+    end
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+          :password_confirmation => "" }
+      end
+
+      it "should not create user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the rigth title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+  end
+
 end
