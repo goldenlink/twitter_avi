4203172e69957661e8e9ff9bfe07edcd530b7fa6
Aurélien VIOLETTE <aurelien.violette@free.fr>
2011-09-14 13:45:06 +0300

    Test registration email with first text email. To be redone: email text and addded html version

--- spec/mailers/post_mailer_spec.rb
+++ spec/mailers/post_mailer_spec.rb
@@ -1,5 +1,41 @@
 require "spec_helper"
 
 describe PostMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Registration notification" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should send registration email" do
+      lambda { PostMailer.registration_confirmation(@user) }.should_not raise_error
+    end
+
+    describe "registration email" do
+
+      before(:each) do
+        @mail = PostMailer.registration_confirmation(@user)
+      end
+
+      it "should have the registration text" do
+        @mail.body.should contain(/registration/)
+      end
+
+      it "should deliver mail sucessfully" do
+        lambda { @mail.deliver }.should_not raise_error
+      end
+
+      describe "should be added to devliering queue" do
+
+        it "should be delivered" do
+          lambda { @mail.deliver }.should
+          change(ActionMailer::Base.deliveries, :size).by(1)
+        end
+
+      end
+    end
+
+  end
+
 end
